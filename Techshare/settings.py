3f96359b4f03508d64ef15a44eff3070fa5c03fb
Maureen Ononiwu <59079323+Chinwendu20@users.noreply.github.com>
2022-06-08 09:32:59 +0100

    Modified settings.py

--- Techshare/settings.py
+++ Techshare/settings.py
@@ -156,15 +156,16 @@ cloudinary.config(
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_URL = 'static/'
 
-STATIC_URL = '/static/'
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 AUTH_USER_MODEL = 'authentication.MyUser'
 
